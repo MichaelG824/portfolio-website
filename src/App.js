5053ac1ccfd49fb3a8d8188d8c36ef47c57809ef
Michael Gee <michaelgee@Michaels-MacBook-Pro.local>
2021-05-08 11:27:28 -0700

    Add fade up when scroll down

--- src/App.js
+++ src/App.js
@@ -6,7 +6,18 @@ import Typical from 'react-typical';
 import LinkedIn from './linkedin.png';
 import Github from './github.png';
 import Resume from './resume.png';
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from 'react';
+import Textpert from './textpert.png';
+import Ucla from './ucla.png';
+import WeWork from './wework.jpeg';
+import Bluebeam from './Bluebeam.png';
 function App() {
+
+  useEffect(() => {
+    Aos.init({duration: 2000 });
+  }, []);
   return (
     <div className="App">
       <div className="intro">
@@ -43,15 +54,21 @@ function App() {
         <img className="github" src={Github} />
         <img className="resume" src={Resume} />
       </div>
-      <FadeIn delay={500}>
-        <div id="about-me">
-          <h1 className="about-me">About Me</h1>
-          <img className="baby-picture" src={babyPicture} />
-          <p className="about-me-paragraph">Ever since I was a kid, I’ve always loved to learn, especially with strategy games. In elementary school I loved chess and in high school, I fell in love with the game of poker. I’ve always loved the process of learning about mistakes and improving.  My love of learning led me to the world of Computer Science and I haven’t looked back since. </p>
-        </div>
-      </FadeIn>
+      <div className="about-me-w" >
+        <h1 className="about-me" data-aos="fade-up">About Me</h1>
+        <img className="baby-picture" src={babyPicture} data-aos="fade-up" />
+        <p className="about-me-paragraph" data-aos="fade-up">Ever since I was a kid, I’ve always loved to learn, especially with strategy games. In elementary school I loved chess and in high school, I fell in love with the game of poker. I’ve always loved the process of learning about mistakes and improving.  My love of learning led me to the world of Computer Science and I haven’t looked back since. </p>
+      </div>
       
-      <div></div>
+      
+      <div id="experience">
+        <h1 className="work-experience" data-aos="fade-up">Work Experience</h1>
+        <p className="textpert-company-name">Textpert SWE Intern</p>
+        <img className="textpert" src={Textpert} data-aos="fade-up" />
+        <img className="ucla" src={Ucla} data-aos="fade-up"/>
+        <img className="wework" src={WeWork} data-aos="fade-up"/>
+        <img className="bluebeam" src={Bluebeam} data-aos="fade-up" />
+      </div>
 
     </div>
   );
